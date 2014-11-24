66c52312b7701ef1a86788ccb6a5aae79b24602c
shenxiaoshuang <247820400@qq.com>
2014-11-24 23:57:03 +0800

    nice

--- a+b.cpp
+++ a+b.cpp
@@ -1,3 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-int String2Number()
+int String2Number(string number);
+int main()
+{
+	string shizi,ca,cb;
+	int a,b;
+	string 
+
+	return 0;
+}
+int String2Number(string number)
+{
+	sring Number[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+	
+}
