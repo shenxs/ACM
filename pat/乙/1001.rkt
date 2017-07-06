e83335ec05a42055e6ee7eb7c44300e92d0b1afc
shenxs <a247820400@gmail.com>
2017-07-06 13:40:49 +0800

    还不错,pat居然还可以用racket

--- pat/乙/1001.rkt
+++ pat/乙/1001.rkt
@@ -6,6 +6,5 @@
     [(odd? n) (claz (/ (+ (* n 3) 1) 2) (add1 i))]
     [else (claz (/ n 2) (add1 i))]))
 
-(let loop ([n (read)])
-  (displayln (claz n 0))
-  (loop (read)))
+(define n (read))
+(display  (claz n 0))
