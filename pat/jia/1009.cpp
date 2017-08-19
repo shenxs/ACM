b2ad31494c11f9dfb5a4a591faf3fdef90434f16
shenxs <a247820400@gmail.com>
2017-08-19 21:23:42 +0800

    删除不必要的头文件

--- pat/jia/1009.cpp
+++ pat/jia/1009.cpp
@@ -1,12 +1,4 @@
 #include<iostream>
-#include<string>
-#include<list>
-#include<vector>
-#include<math.h>
-#include<algorithm>
-#include<sstream>
-#include<stack>
-#include<queue>
 #include<iomanip>
 #include<map>
 using namespace std;
